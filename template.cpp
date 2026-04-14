775588164147e237db58548af8fcee095566ac9d
Stefan Denciu <stefandenciu83@gmail.com>
2026-04-14 20:49:18 +0300

    Add main function with input and sorting logic

---
+++ template.cpp
@@ -0,0 +1,16 @@
+int main() {
+std :: ios :: sync_with_stdio(false);
+std :: cin.tie(nullptr);
+
+int n;
+std: :cin >> n;
+std :: vector<int> a(n);
+for (int& x : a) {
+std: :cin >> x;
+
+custom_sort(a);
+
+assert(std :: ranges :: is_sorted(a));
+return 0;
+
+}
