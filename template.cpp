bee475bdd2657377ea4c2928039827c4fad2fb7f
qué valentin? <95167780+qvalentln@users.noreply.github.com>
2026-04-15 00:21:56 +0300

    template functional

--- template.cpp
+++ template.cpp
@@ -1,16 +1,22 @@
+#include<iostream>
+#include<cassert>
+#include<vector>
+#include<ranges>
+
 int main() {
-std :: ios :: sync_with_stdio(false);
-std :: cin.tie(nullptr);
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
 
-int n;
-std: :cin >> n;
-std :: vector<int> a(n);
-for (int& x : a) {
-std: :cin >> x;
+        int n;
+        std::cin >> n;
+        std::vector<int> a(n);
+        for (int& x : a) {
+                std::cin >> x;
+        }
 
-custom_sort(a);
+        custom_sort(a);
 
-assert(std :: ranges :: is_sorted(a));
-return 0;
+        assert(std::ranges::is_sorted(a));
+        return 0;
 
 }
